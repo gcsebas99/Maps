987f17d4fd4c17c4c1cf21ee000b118b7514bbfe
Sebastian G <gcsebas99@gmail.com>
2020-01-02 16:30:34 -0600

    Movement changes
    Movement changes

---
+++ Assets/Scripts/InteractableObjects/PausedMovement.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using UnityEngine;
+
+public class PausedMovement {
+  public Vector3 pausedPosition;
+  public MovableObject.Direction pausedDirection;
+}
---
+++ Assets/Scripts/InteractableObjects/PendingMovement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class PendingMovement {
+  public Vector3 destination;
+  public bool requiresPause;
+  public Vector3 pausedPosition;
+  public MovableObject.Direction pausedDirection;
+  public int startMovePause;
+  public int endMovePause;
+  public bool generator;
+  public bool pushPullTriggered;
+}
